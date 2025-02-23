2372c68c592b1a9f0904f61e81acc2196ead1d13
Anugrah2334 <anugrahkvs@gmail.com>
2025-02-23 16:04:46 +0530

    Added .github and FAST_API

--- FAST_API/main.py
+++ FAST_API/main.py
@@ -4,11 +4,11 @@ import uvicorn
 
 @app.get("/")
 def read_root():
-    return dict(name = "Prateek", Location = "Deheradun")
+    return dict(name = "Anugrah", Location = "Deheradun")
 
 @app.get("/{data}")
 def read_root(data):
     return dict(hi = data, Location = "Deheradun")
 
-if name == "main":
+if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=80, reload=True)
